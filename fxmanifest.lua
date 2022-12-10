985c027d1dfbd4fd59ba7c6e3a7a9ec9d72176b0
ANTOND <71350868+antond15@users.noreply.github.com>
2022-12-10 13:47:04 +0100

    feat: initial files

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+name 'ac_gifts'
+author 'ANTOND.#8507'
+version '0.0.0'
+description 'A simple script to create Christmas gifts for ox_inventory..'
+repository 'https://github.com/acscripts/ac_gifts'
+
+
+server_scripts {
+  'config.lua',
+  'server/main.lua',
+}
+
+client_script 'client/main.lua'
